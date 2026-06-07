2d926ff4ee87bd5bb3e24d1f2e48c5e7243095f7
suvankars <suvankar.17@gmail.com>
2026-06-07 07:18:32 +0200

    Initial commit

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import typescriptLogo from "/typescript.svg";
+import { Header } from "@repo/ui";
+
+const App = () => (
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" className="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img
+        src={typescriptLogo}
+        className="logo vanilla"
+        alt="TypeScript logo"
+      />
+    </a>
+    <Header title="Web" />
+  </div>
+);
+
+createRoot(document.getElementById("app")!).render(<App />);
---
+++ scripts/start.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer").default;
+const { spawn } = require("child_process");
+
+async function main() {
+    const { apps } = await inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "apps",
+      message: "Select applications to start",
+      choices: [
+        { name: "web", value: "web" },
+        { name: "dashboard", value: "dashboard" },
+      ],
+    },
+  ]);
+
+   if (apps.length === 0) {
+    console.log("No applications selected");
+    process.exit(0);
+  }
+
+  const filters = apps.flatMap(((app) => [`--filter=${app}`]));
+
+  const child = spawn(
+    "turbo",
+    ["dev", ...filters],
+    {
+      stdio: "inherit",
+      shell: true,
+      env: {
+        ...process.env
+      }
+    }
+  );
+
+  child.on("exit", (code) => {
+    process.exit(code ?? 0);
+  });
+}
+
+main();
